9a3736ad492961cea9fa5acee28834fcaed9377a
Nin <anthuylinh999@gmail.com>
2020-04-28 19:49:10 +0700

    commit new

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.set('views', './views');
+
+app.set('view engine', 'ejs');
+
 app.get('/',function(req,res){
   const name = req.query.name||'world';
   res.end(`Hello ${name} from express`);
+});
+
+app.get('/covid19',function(req,res){
+  res.render('covid19');
 });
 
 app.listen(port);
