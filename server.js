930c53670a3995bbcda1c422dde0a18b8f0db289
Nin <anthuylinh999@gmail.com>
2020-05-08 19:03:40 +0700

    sum twos numbers

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // 1760142 - Nguyễn Nhật Ninh
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -8,9 +9,22 @@ app.set('views', './views');
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/',function(req,res){
-  const name = req.query.name||'world';
-  res.end(`Hello ${name} from express`);
+  const name = req.query.name||''
+  res.render('index',{name});
+});
+
+app.get('/sum',function(req,res){
+  res.render('sum');
+});
+
+app.post('/results',function(req,res){
+  const a =Number(req.body.a);
+  const b =Number(req.body.b);
+  const sum =a + b;
+  res.render('results',{a , b, sum});
 });
 
 app.get('/covid19',function(req,res){
